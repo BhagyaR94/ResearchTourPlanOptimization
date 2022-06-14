355c7e4cbd4e90d8ee40b8a29cd3c80c5525262b
BhagyaR94 <rathnayake.bhagya94@gmail.com>
2022-06-14 20:55:28 +0530

    main file modified

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ from src.service import DestinationOptimizerService
 app = Flask(__name__)
 
 
+@app.route('/')
+def root():
+    return 'hello'
+
+
 @app.route('/test')
 def index():
     destination_service = DestinationOptimizerService
     return jsonify(destination_service.get_optimized_destinations())
 
 
-app.secret_key = "secret"
-app.debug = True
-app.run()
+if __name__ == "__main__":
+    app.secret_key = "secret"
+    app.debug = True
+    app.run()
