668508db94d7de112b8bbf9ea714f72859a963f0
BhagyaR94 <rathnayake.bhagya94@gmail.com>
2022-11-30 22:52:49 +0530

    fitness function and optimality of the results improved

--- src/service/DestinationOptimizerService.py
+++ src/service/DestinationOptimizerService.py
@@ -1,11 +1,8 @@
-import datetime
-import json
 import string
-import time
 
 from src.model.Destination import Destination
-from src.util import DistanceUtil
 from src.service import OptimizerCore
+from src.util import DistanceUtil
 from src.util import FileUtils
 
 
@@ -15,22 +12,29 @@ class DestinationOptimizrService:
         self
 
 
-categories = ['adventure', 'animals', 'arts', 'beach', 'buddhism', 'city', 'cruise', 'drinks', 'elephants', 'food',
-              'heritage', 'hiking', 'hunting', 'jungle', 'liquor', 'mountain', 'nature', 'none', 'rafting', 'rivers',
-              'safari', 'scenic', 'sea', 'shooting', 'surfing', 'train', 'waterfall']
-
-
 def get_tuples_from_csv(path: string, selected_categories: []) -> [Destination]:
     data = FileUtils.get_tuples_from_csv(path)
 
-    destinations = []
+    category1_destinations = []
+    category2_destinations = []
+    category3_destinations = []
     for row in data:
-        if set(selected_categories) & {row[1], row[3], row[5]}:
+        if selected_categories[0] == row[1]:
             destination = Destination(row[0], row[1], int(row[2]), row[3], int(row[4]), row[5], int(row[6]),
                                       int(row[7]),
                                       int(row[8]), row[9], float(row[10]), float(row[11]))
-            destinations.append(destination)
-    return destinations
+            category1_destinations.append(destination)
+        if selected_categories[1] == row[1]:
+            destination = Destination(row[0], row[1], int(row[2]), row[3], int(row[4]), row[5], int(row[6]),
+                                      int(row[7]),
+                                      int(row[8]), row[9], float(row[10]), float(row[11]))
+            category2_destinations.append(destination)
+        if selected_categories[2] == row[1]:
+            destination = Destination(row[0], row[1], int(row[2]), row[3], int(row[4]), row[5], int(row[6]),
+                                      int(row[7]),
+                                      int(row[8]), row[9], float(row[10]), float(row[11]))
+            category3_destinations.append(destination)
+    return [category1_destinations, category2_destinations, category3_destinations]
 
 
 def get_optimized_destinations(selected_categories: [], duration_of_stay: int, budget: int) -> string:
@@ -40,9 +44,23 @@ def get_optimized_destinations(selected_categories: [], duration_of_stay: int, b
     mattala = Destination('Mattala Airport', 'none', 0, 'none', 0, 'none', 0, 'destination', 0, 0, 6.292807024583674,
                           81.12278893160014)
 
-    destinations = DistanceUtil.sort_locations_minimum_distance(
-        OptimizerCore.get_optimized_destinations(get_tuples_from_csv('datasets/destinations.csv', selected_categories),
-                                                 duration_of_stay, budget, selected_categories), mattala)
+    category1 = get_tuples_from_csv('datasets/destinations.csv', selected_categories)[0]
+    category2 = get_tuples_from_csv('datasets/destinations.csv', selected_categories)[1]
+    category3 = get_tuples_from_csv('datasets/destinations.csv', selected_categories)[2]
+
+    optimized_destinations_cat1 = DistanceUtil.sort_locations_minimum_distance(
+        OptimizerCore.get_optimized_destinations(category1,
+                                                 ((duration_of_stay*50)/100), ((budget*50)/100), selected_categories), mattala)
+
+    optimized_destinations_cat2 = DistanceUtil.sort_locations_minimum_distance(
+        OptimizerCore.get_optimized_destinations(category2,
+                                                 ((duration_of_stay*30)/100), ((budget*30)/100), selected_categories), mattala)
+
+    optimized_destinations_cat3 = DistanceUtil.sort_locations_minimum_distance(
+        OptimizerCore.get_optimized_destinations(category3,
+                                                 ((duration_of_stay*20)/100), ((budget*20)/100), selected_categories), mattala)
+
+    destinations = optimized_destinations_cat1 + optimized_destinations_cat2 + optimized_destinations_cat3
 
     objects = []
     for destination in destinations:
